507fa867cb1750235780c01b7a1179be325bf9dd
Kevin Schmid <kschmid@Kevins-MacBook-Pro-5.local>
2013-11-02 14:20:30 -0400

    style

--- chat.js
+++ chat.js
@@ -3,19 +3,21 @@ var sio = require('socket.io');
 var connections = [];
 
 sio.listen(1337).sockets.on('connection', function (socket) {
-  socket.emit('new_message', "Welcome to the chat client!");
+    
+    socket.emit('new_message', "Welcome to the chat client!");
   
-  connections.push(socket);
+    connections.push(socket);
 
-  socket.on('user_sent', function (data) {
-    for (var i = 0; i < connections.length; i++)
-    {
-        if (connections[i] != socket)
+    socket.on('user_sent', function (data) {
+    
+        for (var i = 0; i < connections.length; i++)
         {
-            connections[i].emit('new_message', data);
+            if (connections[i] != socket)
+            {
+                connections[i].emit('new_message', data);
+            }
         }
-    }
 
-  });
+    });
 
 });
