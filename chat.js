ebdcdbba15ff29e1454b48195734e4638420ed9f
Kevin Schmid <kschmid@Kevins-MacBook-Pro-5.local>
2013-11-02 02:55:10 -0400

    Adding simple chat server.

---
+++ chat.js
@@ -0,0 +1,21 @@
+var sio = require('socket.io');
+
+var connections = [];
+
+sio.listen(1337).sockets.on('connection', function (socket) {
+  socket.emit('new_message', "Welcome to the chat client!");
+  
+  connections.push(socket);
+
+  socket.on('user_sent', function (data) {
+    for (var i = 0; i < connections.length; i++)
+    {
+        if (connections[i] != socket)
+        {
+            connections[i].emit('new_message', data);
+        }
+    }
+
+  });
+
+});
